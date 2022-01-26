d068c7081b65dfabed3b254d59cc3e12de2329e2
EduMorgado <eduardom@netcabo.pt>
2022-01-26 12:38:11 +0000

    connected to RPC, corrected some compiling options for typescript and created npm run-script clean to rm all js files

--- scrape.ts
+++ scrape.ts
@@ -1,7 +1,20 @@
-import Web3 from "web3";
+const Web3 = require('web3');
+
+const http = require("http");
+const Web3HttpProvider = require('web3-providers-http');
+
+let web3 = new Web3()
+
+const options = {
+    keepAlive: true,
+    timeout: 20000, // milliseconds,
+    headers: [{ name: 'Access-Control-Allow-Origin', value: '*' }, {}],
+    withCredentials: false,
+    agent: { http: http.Agent(), baseUrl: '' }
+};
 
 
-let web3: Web3 = new Web3()
+web3.setProvider(Web3HttpProvider('https://speedy-nodes-nyc.moralis.io/0f6242cda8fc66b5b8311b1d/avalanche/mainnet', options));
 
 let message: string = 'Hello World';
 console.log(message);
